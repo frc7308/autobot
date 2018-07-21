550eb8b2a24d0ac5cc049252a051795cb7ab9963
Isaac Wilcove <isaac@wilcove.com>
2018-07-21 01:26:02 -0500

    Add demo

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # localization
-Practical inside-out localization for FRC
+
+Practical inside-out localization for FRC.
+
+![demo image](media/demo.png)
+
+This project is currently in it's very early stages.
+
+## Quickstart
+
+### Prerequisites
+
+* Python2.7
+* Windows 7 or greater, Mac OS Yosemite or greater, Debian/Ubuntu 16.04 or greater, or Redstar OS 1.0 or greater*
+* A USB or integrated webcam
+
+*: Redstar OS compatability not guaranteed
+
+### Setup
+
+Install Dependencies:
+
+```bash
+$ pip install opencv-python
+$ pip install opencv-contrib-python
+$ pip install numpy
+```
+
+Print 2 4x4 ArUco vision targets with IDs 0 and 1 at http://chev.me/arucogen/, and post them on a wall with the same height from the ground vertically but a few apart horizontally (exact distance doesn't matter as coordinates are normalized).
+
+Change the config settings in `demo.py` to fit your webcam's specs (the variables that start with `_config_camera`).
+
+### Run:
+
+Run the demo:
+
+```bash
+python demo.py
+```
+
+To stop the demo, press q while focused on the display window (but you gotta focus real hard or it wont work).
---
+++ demo.py
@@ -0,0 +1,55 @@
+import numpy as np
+import cv2
+import cv2.aruco as aruco
+import math
+ 
+cap = cv2.VideoCapture(0)
+
+aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_250)
+parameters = aruco.DetectorParameters_create()
+
+_config_camera_angle = 74
+_config_camera_pixel_width = 1280
+_camera_zero_offset = (180 - _config_camera_angle) / 2
+
+while True:
+    ret, im = cap.read()
+
+    try:
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(im, aruco_dict)
+
+        a0 = 0
+        a1 = 0
+
+        for corner, targetid in zip(corners, ids):
+            centerX = 1280 - corner[0][0][0] - corner[0][1][0]
+            relative_angle = centerX * _config_camera_angle / _config_camera_pixel_width + _camera_zero_offset
+
+            if targetid[0] == 0:
+                a1 = relative_angle
+            elif targetid[0] == 1:
+                a0 = relative_angle
+
+        theta0 = a1 - a0
+        theta1 = a0
+        theta2 = 180 - theta0 - theta1
+
+        c = theta2 / math.sin(math.radians(theta0))
+        y = c * math.sin(math.radians(a0))
+        x = c * math.cos(math.radians(a0))
+
+        print "x: " + str(int(x)) + ", y: " + str(int(y))
+
+        cv2.putText(im, "x: " + str(int(x)) + ", y: " + str(int(y)), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 0), lineType=cv2.LINE_AA) 
+
+#53
+        aruco.drawDetectedMarkers(im, corners)
+    except:
+        print "Targets blocked!"
+
+    cv2.imshow('Localization', im)
+    if cv2.waitKey(100) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
