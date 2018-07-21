9c23163ff19ac8803f7c42a07814ebbaef336900
Isaac Wilcove <isaac@wilcove.com>
2018-07-21 18:19:13 -0500

    UI improvements

--- demo.py
+++ demo.py
@@ -12,42 +12,66 @@ _config_camera_angle = 74
 _config_camera_pixel_width = 1280
 _camera_zero_offset = (180 - _config_camera_angle) / 2
 
-_target_dist = 51.5 # Inches
+_target_dist = 39 # Inches
 
 while True:
-    ret, im = cap.read()
+    ret, frame = cap.read()
+
+    im = np.zeros((650, 1000, 3), dtype=np.uint8)
+
+    im[0:frame.shape[0], 0:frame.shape[1]] = frame
 
     try:
         corners, ids, rejectedImgPoints = aruco.detectMarkers(im, aruco_dict)
 
-        a0 = 0
-        a1 = 0
+        if len(corners) < 2:
+            print "Targets not found"
+            cv2.putText(im, "Targets not found", (40, 575), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 0), lineType=cv2.LINE_AA) 
+        else:
 
-        for corner, targetid in zip(corners, ids):
-            centerX = 1280 - corner[0][0][0] - corner[0][1][0]
-            relative_angle = centerX * _config_camera_angle / _config_camera_pixel_width + _camera_zero_offset
+            a0 = 0
+            a1 = 0
 
-            if targetid[0] == 0:
-                a1 = relative_angle
-            elif targetid[0] == 1:
-                a0 = relative_angle
+            for corner, targetid in zip(corners, ids):
+                centerX = 1280 - corner[0][0][0] - corner[0][1][0]
+                relative_angle = math.radians(centerX * _config_camera_angle / _config_camera_pixel_width + _camera_zero_offset)
 
-        theta0 = a1 - a0
-        theta1 = a0
-        theta2 = 180 - theta0 - theta1
+                if targetid[0] == 0:
+                    a1 = relative_angle
+                elif targetid[0] == 1:
+                    a0 = relative_angle
 
-        c = theta2 / math.sin(math.radians(theta0))
-        y = c * math.sin(math.radians(a0)) / 90 * _target_dist
-        x = c * math.cos(math.radians(a0)) / 90 * _target_dist
+            theta0 = a1 - a0
+            theta1 = a0
+            theta2 = math.pi - theta0 - theta1
 
-        print "x: " + str(int(x)) + ", y: " + str(int(y))
+            c = theta2 / math.sin(theta0)
+            y = c * math.sin(a0) / (math.pi / 2) * _target_dist
+            x = c * math.cos(a0) / (math.pi / 2) * _target_dist
 
-        cv2.putText(im, "x: " + str(int(x)) + ", y: " + str(int(y)), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), lineType=cv2.LINE_AA) 
+            print "x: " + str(x) + ", y: " + str(y)
 
-#53
-        aruco.drawDetectedMarkers(im, corners)
+            cv2.putText(im, "(" + str(int(x)) + ", " + str(int(y)) + ")", (40, 575), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 0), lineType=cv2.LINE_AA)
+
+            xDraw = (_target_dist - int(x)) * 4 + 750
+            yDraw = int(y) * 4 + 40
+
+            cv2.rectangle(im, (730, 20), (_target_dist * 4 + 770, 500), (200, 200, 200), 3)
+
+            cv2.circle(im, (750, 40), 4, (255, 0, 0), 8)
+            cv2.circle(im, (_target_dist * 4 + 750, 40), 4, (255, 0, 0), 8)
+
+            cv2.circle(im, (xDraw, yDraw), 4, (0, 0, 255), 8)
+
+            cv2.line(im, (750, 40), (xDraw, yDraw), (255, 0, 255), 2)
+            cv2.line(im, (_target_dist * 4 + 750, 40), (xDraw, yDraw), (255, 0, 255), 2)
+            cv2.line(im, (750, 40), (_target_dist * 4 + 750, 40), (255, 0, 255), 2)
+
+            cv2.line(im, (frame.shape[1] / 2, 0), (frame.shape[1] / 2, frame.shape[0] - 1), (255, 255, 255), 1)
+
+            aruco.drawDetectedMarkers(im, corners)
     except:
-        print "Targets blocked!"
+        print "Targets not found"
 
     cv2.imshow('Localization', im)
     if cv2.waitKey(100) & 0xFF == ord('q'):
